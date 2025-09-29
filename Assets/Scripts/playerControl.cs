0190ba55ceab43a67ef48645f8c00c5323a96802
Maxinudel <maxinudel08@gmail.com>
2025-09-29 14:25:24 -0300

    intentando buscador y buscado

---
+++ Assets/Scripts/playerControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class playerControl : MonoBehaviourPun
+{
+   [SerializeField] private GameObject buscadorSkin;
+   [SerializeField] private GameObject buscadoSkin;
+
+   private empezarPartida partida;
+
+   void Start()
+   {
+        partida = FindObjectOfType<empezarPartida>();
+        ActualizarSkin();
+    }
+
+    public void ActualizarSkin()
+    {
+        Photon.Realtime.Player buscador = partida.GetBuscador();
+
+        if (PhotonNetwork.LocalPlayer == buscador)
+        {
+            buscadorSkin.SetActive(true);
+            buscadoSkin.SetActive(false);
+        }
+        else
+        {
+            buscadorSkin.SetActive(false);
+            buscadoSkin.SetActive(true);
+        }
+    }
+}
